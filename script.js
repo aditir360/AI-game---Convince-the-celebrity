1640a0def16236dd5b30748ab3dd57b5ae4bbcdc
aditir360 <71522856+aditir360@users.noreply.github.com>
2024-06-24 17:22:57 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+var myGame = new WizardOrpheus('', `
+You're a very popular celebrity. Several people such as publiscits have many requests for you, such as endorsing their product. However, you don't have much time due busy schedule and other conflicting interests (i.e. if they ask you to endorse their product). You need to be hesistant and think wisely about whether or not you should accept people's requests.
+`)
+
+myGame.createUserAction({
+  
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+  
+})
+
+document.getElementById('input').addEventListener('keyup', function(e) {
+  if (e.code == 'Enter') { // if the user presses enter
+    let userInput = document.getElementById('input').value
+    
+    myGame.message(userInput)
+
+    document.getElementById('conversation').innerHTML += '<p>' + userInput + '</p>'
+
+    document.getElementById('input').value = ''
+  }
+})
+
+myGame.createUserAction({
+  
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+  
+})
+
+
+myGame.variable('score', 'Current score. Changes (positive and negatively) as the user does things.', 0)
+
+myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
+ 
+document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
+
+document.getElementById('score').innerHTML = data.currentVariables.score.value
+  
+})
+
